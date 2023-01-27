be0e6fe5e3798cdf7ee9f1136fb13b3cac16e8e0
wesleymktd <wesleymktd@gmail.com>
2023-01-27 14:44:11 -0300

    requisito 10 e inicio 11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import FetchContext from '../context/FetchContext';
 
 function SearchBar() {
+  const [searchParams, setSearchParams] = useState({
+    searchInput: '',
+    typeSearch: '',
+  });
+  const { requestFetch } = useContext(FetchContext);
+
   useEffect(() => {
   }, []);
 
+  const handleChange = ({ target: { name, value } }) => {
+    setSearchParams({
+      ...searchParams,
+      [name]: value,
+    });
+  };
+
+  const fetchSubmit = () => {
+    requestFetch(searchParams);
+  };
+
   return (
-    <input
-      type="text"
-      data-testid="search-input"
-    />
+    <div>
+      <input
+        type="text"
+        name="searchInput"
+        data-testid="search-input"
+        onChange={ handleChange }
+        value={ searchParams.searchInput }
+      />
+      <br />
+      <label htmlFor="search-ingredient">
+        <input
+          id="search-ingredient"
+          data-testid="ingredient-search-radio"
+          type="radio"
+          name="typeSearch"
+          value="ingredient"
+          onChange={ handleChange }
+        />
+        Ingredient
+      </label>
+
+      <label htmlFor="search-name">
+        <input
+          id="search-name"
+          data-testid="name-search-radio"
+          type="radio"
+          name="typeSearch"
+          value="Name"
+          onChange={ handleChange }
+        />
+        Name
+      </label>
+
+      <label htmlFor="search-name">
+        <input
+          id="search-name"
+          data-testid="first-letter-search-radio"
+          type="radio"
+          name="typeSearch"
+          value="First letter"
+          onChange={ handleChange }
+        />
+        First letter
+      </label>
+      <br />
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+        onClick={ fetchSubmit }
+      >
+        SEARCH
+      </button>
+    </div>
   );
 }
 
