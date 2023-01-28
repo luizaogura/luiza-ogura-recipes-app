7d35e802cc8e1df457c9061fcb3613862c95f848
hildelio <hildelio@gmail.com>
2023-01-28 02:55:29 -0300

    fix lint :detective:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import FetchContext from '../context/FetchContext';
 
