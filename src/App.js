a8d7fef9f2ede52a321a2781e7019e4f59dbaad6
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-01-31 17:30:17 -0300

    fixing branch

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import LoginProvider from './context/LoginProvider';
+import FetchProvider from './context/FetchProvider';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeInProgress from './pages/RecipeInProgress';
+// import Recipes from './pages/Recipes';
+// import RecipesProvider from './context/RecipesProvider';
+
+function App() {
+  return (
+    <LoginProvider>
+      <FetchProvider>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ DoneRecipes } />
+          <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+          <Route
+            exact
+            path="/meals/:id/in-progress"
+            component={ RecipeInProgress }
+          />
+          <Route
+            exact
+            path="/drinks/:id/in-progress"
+            component={ RecipeInProgress }
+          />
+        </Switch>
+      </FetchProvider>
+    </LoginProvider>
+  );
+}
+
+export default App;
