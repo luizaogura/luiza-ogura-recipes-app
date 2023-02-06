ae5f447449fe6e432351ebcc5f2092da26a137ab
Anderson Sales <azkarsb1511@gmail.com>
2023-02-06 20:49:31 -0300

    req 44, 45, 46, 47, 48, 49 e sharebutton

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
-import Header from '../components/Header';
+import { React, useState, useEffect } from 'react';
+// import Header from '../components/Header';
+import CardDoneRecipes from '../components/CardDoneRecipes';
 
 function DoneRecipes() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [recipesToRender, setRecipesToRender] = useState(doneRecipes);
+  const [filterButton, setFilterButton] = useState('clear');
+
+  const handleFilterClick = ({ target: { name } }) => {
+    setFilterButton(name);
+  };
+
+  const filtering = (filter) => {
+    const recipesFiltered = {
+      clear: doneRecipes,
+      meal: doneRecipes.filter((recipe) => recipe.type === 'meal'),
+      drink: doneRecipes.filter((recipe) => recipe.type === 'drink'),
+    };
+    return recipesFiltered[filter];
+  };
+
+  useEffect(() => {
+    setRecipesToRender(filtering(filterButton));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filterButton]);
+
   return (
     <div>
-      <Header />
+
+      <button
+        data-testid="filter-by-all-btn"
+        name="clear"
+        onClick={ handleFilterClick }
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        name="meal"
+        onClick={ handleFilterClick }
+      >
+        Meals
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        name="drink"
+        onClick={ handleFilterClick }
+      >
+        Drinks
+      </button>
+      { doneRecipes && recipesToRender.map((recipe, index) => (
+        <CardDoneRecipes
+          key={ recipe.id }
+          recipe={ recipe }
+          index={ index }
+        />))}
     </div>
+
   );
 }
 
